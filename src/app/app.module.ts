57a527c08af98a6cb31fac36382dcb0ed3d225e5
SalifAbdoulSow18 <abdoulsalifsow06@gmail.com>
2022-08-31 15:46:52 +0000

    mis à jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DetailsComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DetailsComponent } from '../details/details.component';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  Articles: any[];
+  private navCtrl: any;
 
-  constructor() {}
+  constructor() {
+    this.Articles = [
+      {nom: 'Télevision', prix: 145, details: 'Lorem ipsum de Télevision'},
+      {nom: 'Téléphone', prix: 130, details: 'Lorem ipsum de Téléphone'},
+      {nom: 'Sac à dos', prix: 11, details: 'Lorem ipsum de Sac à dos'},
+      {nom: 'PC', prix: 800, details: 'Lorem ipsum de PC'},
+      {nom: 'Radio', prix: 33, details: 'Lorem ipsum de Radio'},
+    ];
+  }
+
+  showDetails(): void {
+    this.navCtrl.push(DetailsComponent);
+  }
 
 }
